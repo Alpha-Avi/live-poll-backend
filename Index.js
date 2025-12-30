e2de4a065d3de223056bb957931eac8d7e8f7f6d
Alpha-Avi <mywork.avi.0614@gmail.com>
2025-12-30 13:48:14 +0530

    fix render port and add root route

--- Index.js
+++ Index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.get("/", (req, res) => {
-  res.send("Live Poll Backend Running 🚀");
+  res.send("Live Poll Backend Running ");
 });
 
 
